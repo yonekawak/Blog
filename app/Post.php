ba34fd4d283cbe51ee330f98f982f7fa7f7e2115
yonekawak <d3112101@stu.kogakkan-u.ac.jp>
2022-01-25 15:53:46 +0900

    [add]記事一覧画面

---
+++ app/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    public function getByLimit(int $limit_count = 10)
+    {
+        // updated_atで降順に並べたあと、limitで件数制限をかける
+        return $this->orderBy('updated_at', 'DESC')->limit($limit_count)->get();
+    }
+}
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,13 +9,12 @@
     <body>
         <h1>Blog Name</h1>
         <div class='posts'>
-            @foreach($posts as $post)
+            @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $posts->title }}</h2>
+                    <h2 class='title'>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
         </div>
     </body>
 </html>
-
