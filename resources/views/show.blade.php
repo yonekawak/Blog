8603b2020023479a86e6fd5a17ee61a7625467c6
yonekawak <d3112101@stu.kogakkan-u.ac.jp>
2022-01-26 14:24:14 +0900

    [add]記事の編集

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -9,17 +9,13 @@
         <link rel="stylesheet" href="/css/app.css">
     </head>
     <body>
-        <h1 class="title">
-            {{ $post->title }}
-        </h1>
-        <div class="content">
-            <div class="content__post">
-                <h3>本文</h3>
-                <p>{{ $post->body }}</p>    
-            </div>
+        <h1>Blog Name</h1>
+        <p class='edit'>[<a href="/posts/{{ $post->id }}/edit">edit</a>]</p>
+        <div class='post'>
+            <h2 class='title'>{{ $post->title }}</h2>
+            <p calss='body'>{{ $post->body }}</p>
+            <p calss='updated_at'>{{ $post->updated_at }}</p>
         </div>
-        <div class="footer">
-            <a href="/">戻る</a>
-        </div>
+        <div class='back'>[<a href="/">back</a>]</div>
     </body>
 </html>
